cebde5141c34ed1044eca412dece4c8ca5facc3a
Konrad Gnat <konradmgnat@gmail.com>
2024-11-07 20:11:08 +0700

    init

---
+++ AppTemplate/AppTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppTemplateApp.swift
+//  AppTemplate
+//
+//  Created by Konrad Gnat on 11/7/24.
+//
+
+import SwiftUI
+
+@main
+struct AppTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
